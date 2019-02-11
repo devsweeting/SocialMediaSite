27fd193b04ea05f0e5fb5f45086510bd84372e63
charley mcgowan and christopher cahill <charley+christopher@>
2019-02-11 08:38:00 -0800

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Other from "./Other";
+
+
+
+function App(props) {
+  var myStyledComponentStyles = {
+
+  }
+  return (
+    <div style={myStyledComponentStyles}>
+      <Other/>
+
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Other.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+function Other(props) {
+  var myStyledComponentStyles = {
+
+  }
+  return (
+    <div style={myStyledComponentStyles}>
+      <h1>testing</h1>
+
+      <hr/>
+    </div>
+  );
+}
+
+export default Other;
